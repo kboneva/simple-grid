6a17776c7981b73dd4b659b1f8a6bdc0f3fd0401
Krisi Boneva <krisyyy98@gmail.com>
2024-08-22 01:16:52 +0300

    Frontend update: configure api and services

---
+++ frontend/src/services/buttonService.js
@@ -0,0 +1,39 @@
+const api_url = '/api/buttons';
+
+export const getAll = async () => {
+    const response = await fetch(api_url);
+    return response.json();
+}
+
+export const getButton = async (id) => {
+    const response = await fetch(`${api_url}/${id}`);
+    return response.json();
+}
+
+export const createButton = async (data) => {
+    const response = await fetch(api_url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+export const editButton = async (id, data) => {
+    const response = await fetch(`${api_url}/${id}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+export const deleteButton = async (id) => {
+    await fetch(`${api_url}/${id}`, {
+        method: "DELETE"
+    });
+}
